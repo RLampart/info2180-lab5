e44ba8bcec9af7597007ef3d02c06f31859ce833
rahiemlampart@yahoo.com <rahiemlampart@yahoo.com>
2022-11-11 10:26:43 -0500

    Complete World Database

--- world.js
+++ world.js
@@ -1,8 +1,11 @@
 document.addEventListener("DOMContentLoaded",function(){
     var button = document.getElementById("lookup");
+    var button2 = document.getElementById("lookupct");
     var list = "world.php?country=";
+    var exlist;
     button.onclick = function(event){
         event.preventDefault();
+        exlist ='&lookup=';
         var httpRequest = new XMLHttpRequest();
         httpRequest.onreadystatechange = function(){
             if (httpRequest.readyState == XMLHttpRequest.DONE){
@@ -18,16 +21,41 @@ document.addEventListener("DOMContentLoaded",function(){
         query.trim();
         if((/^[a-zA-Z ]+$/).test(query)){
           list += query;
-        }
-        else if(query == ""){
-          list = list;
+          list += exlist;
         }
         else{
-          list += "invalid";
+          list += exlist;
         }
         httpRequest.open("GET",list);
         httpRequest.send();
         list = "world.php?country="
     }
+    button2.onclick = function(event){
+      event.preventDefault();
+      exlist ='&lookup=cities';
+      var httpRequest = new XMLHttpRequest();
+      httpRequest.onreadystatechange = function(){
+          if (httpRequest.readyState == XMLHttpRequest.DONE){
+             if (httpRequest.status == 200){
+                var response = httpRequest.responseText;
+                var result = document.getElementById("result");
+                result.innerHTML = response;
+          }     
+             else
+                alert("error");}
+      }
+      query = document.getElementById("country").value;
+      query.trim();
+      if((/^[a-zA-Z ]+$/).test(query)){
+        list += query;
+        list += exlist;
+      }
+      else{
+        list += exlist;
+      }
+      httpRequest.open("GET",list);
+      httpRequest.send();
+      list = "world.php?country="
+  }
 
 })
--- world.php
+++ world.php
@@ -4,31 +4,56 @@ $username = 'lab5_user';
 $password = 'password123';
 $dbname = 'world';
 
-$country = $_GET['country'];
+$country = htmlspecialchars($_GET['country']);
+$cities = htmlspecialchars($_GET['lookup']);
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-//$stmt = $conn->query("SELECT * FROM countries");
+if (!empty($country) && $cities == 'cities'){
+   $stmt = $conn->prepare("SELECT c.name,c.district,c.population FROM cities c join countries cs on c.country_code = cs.code WHERE cs.name LIKE :country");
+   $country = "%$country%";
+   $stmt->bindParam(":country",$country);
+   $stmt->execute();
+   $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+   echo '<table>';
+   echo '<tr>';
+   echo '<th> Name </th>';
+   echo '<th> District </th>';
+   echo '<th> Population </th>';
+   echo '</tr>';
+   foreach ($results as $row): 
+    echo '<tr>';
+    echo '<td>'.$row['name'].'</td>';
+    echo '<td>'.$row['district'].'</td>';
+    echo '<td>'.$row['population'].'</td>';
+    echo '</tr>';
+   endforeach; 
+   echo '</table>';
+}
+else if (empty($cities)){
+   $stmt = $conn->prepare("SELECT * FROM countries WHERE name LIKE :country");
+   $country = "%$country%";
+   $stmt->bindParam(":country",$country);
+   $stmt->execute();
+   $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+   echo '<table>';
+   echo '<tr>';
+   echo '<th> Name </th>';
+   echo '<th> Continent </th>';
+   echo '<th> Independence </th>';
+   echo '<th> Head of State </th>';
+   echo '</tr>';
+   foreach ($results as $row): 
+    echo '<tr>';
+    echo '<td>'.$row['name'].'</td>';
+    echo '<td>'.$row['continent'].'</td>';
+    echo '<td>'.$row['independence_year'].'</td>';
+    echo '<td>'.$row['head_of_state'].'</td>';
+    echo '</tr>';
+   endforeach; 
+   echo '</table>';
+}
+else
+   echo '<h3>No country entered</h3>';
 
-//$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
-
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
  
-<table>
-  <tr>
-    <th> Name </th>
-    <th> Continent </th>
-    <th> Independence </th>
-    <th> Head of State </th>
-  </tr>
-<?php foreach ($results as $row): ?>
-  <tr>
-  <td><?= $row['name']; ?></td>
-  <td><?= $row['continent']; ?></td>
-  <td><?= $row['independence_year']; ?></td>
-  <td><?= $row['head_of_state']; ?></td>
-</tr>
-<?php endforeach; ?>
-</table>
-
